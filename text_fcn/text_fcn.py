b044afca39c99d1c5585448beeb078b1d9001aab
M Rota <dubvulture1963@gmail.com>
2017-09-23 13:36:28 +0200

    added support for focused scene text evaluation

--- text_fcn/text_fcn.py
+++ text_fcn/text_fcn.py
@@ -148,6 +148,7 @@ class TextFCN(object):
             for i, fname in enumerate(filenames):
                 in_path = os.path.join(directory, fname)
                 in_image = cv2.imread(in_path + '.jpg')
+                in_image = cv2.resize(in_image, None, fx=0.5, fy=0.5)
                 # pad image to the nearest multiple of 32
                 dy, dx = tf_utils.get_pad(in_image)
                 in_image = tf_utils.pad(in_image, dy, dx)
